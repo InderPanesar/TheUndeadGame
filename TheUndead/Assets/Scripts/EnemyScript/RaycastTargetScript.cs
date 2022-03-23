d0485256e0347a242f10dae2cc9d2b51ab47f992
Inder Panesar <Inder_Panesar@hotmail.com>
2022-03-23 22:38:52 +0000

    Remove lootlocker dependency

--- TheUndead/Assets/Scripts/EnemyScript/RaycastTargetScript.cs
+++ TheUndead/Assets/Scripts/EnemyScript/RaycastTargetScript.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using LootLocker.Requests;
 using UnityEngine.AI;
 using Photon.Pun;
 
